25a02068745eb19a2af8da1008a9ee2601c9da04
jae8259 <dragonj5290@gmail.com>
2022-11-22 18:56:51 +0900

    Convert scripts to notebooks

--- Readme.md
+++ Readme.md
@@ -1,2 +1,9 @@
 https://pypi.org/project/ipynb-py-convert/
 bash convertScriptToNotebook.bash <modelname>
+
+### Direcotry Convention
+
+src/<model name>
+model.py
+preprocess.py
+train.py
--- convertScriptToNotebook.bash
+++ convertScriptToNotebook.bash
@@ -1,4 +1,12 @@
-file="$1"
-echo Converting $file.py to notebook
-ipynb-py-convert src/models/$file.py src/models/$file.ipynb
-echo Finished
+model="$1"
+echo Converting $model to notebook
+
+if [ ! -d "src/${model}_nb/" ]; then
+  mkdir "src/${model}_nb/" 
+fi
+
+for filepath in src/$model/*.py; do
+    ipynb-py-convert "${filepath%.*}".py "${filepath%.*}".ipynb
+    filename=$(basename -- "$filepath")
+    mv "${filepath%.*}".ipynb "src/${model}_nb/${filename%.*}.ipynb"
+done
